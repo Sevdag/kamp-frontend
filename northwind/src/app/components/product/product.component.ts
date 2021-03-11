a47fb2855bf288c76ba092fad6db75fd5440a5b9
S_Gunes <gunesevdaa@gmail.com>
2021-03-11 18:27:10 +0200

    11Mart21

--- northwind/src/app/components/product/product.component.ts
+++ northwind/src/app/components/product/product.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
-import {HttpClient} from '@angular/common/http';
+
 import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -10,17 +11,18 @@ import { ProductResponseModel } from 'src/app/models/productResponseModel';
 })
 export class ProductComponent implements OnInit {
   products:Product[] = [];
-  apiUrl="https://localhost:44369/api/products/getall"
+ dataLoaded = false;
 
-  constructor(private httpClient:HttpClient) {}
+  constructor(private productService:ProductService) {}
 
   ngOnInit(): void {
    this.getProducts();
   }
 
   getProducts(){
-    this.httpClient.get<ProductResponseModel>(this.apiUrl).subscribe((response) => {
-      this.products = response.data
-    });
+  this.productService.getProducts().subscribe(response=>{
+    this.products = response.data
+    this.dataLoaded = true;
+  })
   }
 }
