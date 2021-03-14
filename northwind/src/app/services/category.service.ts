2174472b0257f70c1217a782848a891ce56f99e1
S_Gunes <gunesevdaa@gmail.com>
2021-03-14 10:41:24 +0200

    140321

--- northwind/src/app/services/category.service.ts
+++ northwind/src/app/services/category.service.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/ListRepsonseModel';
-import { Product } from '../models/product';
+import { Category } from '../models/category';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {
+export class CategoryService {
 
-  apiUrl="https://localhost:44369/api/products/getall"
+  apiUrl="https://localhost:44369/api/categories/getall"
 
   constructor(private httpClient:HttpClient) { }
   
-  getProducts():Observable<ListResponseModel<Product>>{
-   return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+  getCategories():Observable<ListResponseModel<Category>>{
+   return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
     
   }
 }
