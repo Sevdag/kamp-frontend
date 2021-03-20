138b33f2277dbad90c4f83fdd2948264199558e9
S_Gunes <gunesevdaa@gmail.com>
2021-03-20 23:45:46 +0200

    200321

--- northwind/src/app/components/product-add/product-add.component.ts
+++ northwind/src/app/components/product-add/product-add.component.ts
@@ -1,38 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormBuilder, FormControl, Validators} from "@angular/forms"
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { ProductService } from 'src/app/services/product.service';
-
 
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
-  styleUrls: ['./product-add.component.css']
+  styleUrls: ['./product-add.component.css'],
 })
 export class ProductAddComponent implements OnInit {
-
-  productAddForm : FormGroup;
-  constructor(private formBuilder:FormBuilder, private productService:ProductService, private toastrService:ToastrService) { }
+  productAddForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private productService: ProductService,
+    private toastrService: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.createProductAddForm();
   }
 
-  createProductAddForm(){
-     this.productAddForm = this.formBuilder.group({
-       productName:["",Validators.required],
-       unitPrice: ["",Validators.required],
-       unitsInStock:["", Validators.required],
-       categoryId:["",Validators.required]
-     })
+  createProductAddForm() {
+    this.productAddForm = this.formBuilder.group({
+      productName: ['', Validators.required],
+      unitPrice: ['', Validators.required],
+      unitsInStock: ['', Validators.required],
+      categoryId: ['', Validators.required],
+    });
   }
-add(){
-if(this.productAddForm.valid){
-  let productModel = Object.assign({},this.productAddForm.value)
-  this.productService.add(productModel)
-  this.toastrService.success("Ürün eklendi","Başarılı")
-}else{
-this.toastrService.error("Formunuz eksik", "Dikkat")
-}
-}
+  add() {
+    if (this.productAddForm.valid) {
+      let productModel = Object.assign({}, this.productAddForm.value);
+      this.productService.add(productModel).subscribe(
+        (response) => {
+          this.toastrService.success(response.message, 'Başarılı');
+        },
+        (responseError) => {
+          if (responseError.error.Errors.length > 0) {
+            for (let i = 0; i < responseError.error.Errors.length; i++) {
+              this.toastrService.error(
+                responseError.error.Errors[i].ErrorMessage,
+                'Doğrulama hatası'
+              );
+            }
+          }
+        }
+      );
+    } else {
+      this.toastrService.error('Formunuz eksik', 'Dikkat');
+    }
+  }
 }
